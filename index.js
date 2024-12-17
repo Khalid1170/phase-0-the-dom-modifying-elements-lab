c638a226777ed572518c9ded355b4c28ccb52a6a
Khalid <Khalid>
2024-12-17 16:02:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "Arsenal FC"
+newHeader.innerHTML = "They have finally won the premier leaague!!";
+newHeader.className = "It has been a very long time ";
